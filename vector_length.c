cce695be3f3f9be0e429e104b431ac67dc9bf68e
dTomnyuk <kn25-d.tomniuk@nubip.edu.ua>
2026-07-04 23:31:43 +0300

    Create vector_length.c

---
+++ vector_length.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double x1, y1, x2, y2;
+
+    if (scanf("%lf %lf %lf %lf", &x1, &y1, &x2, &y2) == 4) {
+ 
+        double dx = x2 - x1;
+        double dy = y2 - y1;
+  
+        double length = sqrt(dx * dx + dy * dy);
+  
+        printf("%.6lf\n", length);
+        
+    }
+    
+    return 0;
+}
